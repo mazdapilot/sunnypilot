b7ca81051c80614d431f0f7e351c4192cc09def0
MazdaNick <nick.tompk@gmail.com>
2025-11-17 16:11:02 -0500

    fix mazda mrcc and standstill

--- opendbc_repo/opendbc/car/mazda/carstate.py
+++ opendbc_repo/opendbc/car/mazda/carstate.py
@@ -2,7 +2,7 @@ from opendbc.can import CANDefine, CANParser
 from opendbc.car import Bus, create_button_events, structs
 from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.interfaces import CarStateBase
-from opendbc.car.mazda.values import DBC, LKAS_LIMITS
+from opendbc.car.mazda.values import DBC, LKAS_LIMITS, CAR
 
 from opendbc.sunnypilot.car.mazda.carstate_ext import CarStateExt
 
@@ -72,7 +72,12 @@ class CarState(CarStateBase, CarStateExt):
     ret.gasPressed = cp.vl["ENGINE_DATA"]["PEDAL_GAS"] > 0
 
     # Either due to low speed or hands off
-    lkas_blocked = cp.vl["STEER_RATE"]["LKAS_BLOCK"] == 1
+    lkas_blocked_raw = cp.vl["STEER_RATE"]["LKAS_BLOCK"] == 1
+    lkas_blocked = lkas_blocked_raw
+    if self.CP.carFingerprint == CAR.MAZDA_CX5_2022:
+      # CX-5 2022 keeps torque available until ~5 mph even though LKAS_BLOCK is raised
+      if ret.standstill or speed_kph < 8.0:
+        lkas_blocked = False
 
     if self.CP.minSteerSpeed > 0:
       # LKAS is enabled at 52kph going up and disabled at 45kph going down
@@ -105,7 +110,11 @@ class CarState(CarStateBase, CarStateExt):
     # Check if LKAS is disabled due to lack of driver torque when all other states indicate
     # it should be enabled (steer lockout). Don't warn until we actually get lkas active
     # and lose it again, i.e, after initial lkas activation
-    ret.steerFaultTemporary = self.lkas_allowed_speed and lkas_blocked
+    if self.CP.carFingerprint in [CAR.MAZDA_CX9_2021, CAR.MAZDA_CX5_2022]:
+      ret.steerFaultTemporary = False
+    else:
+      # On if no driver torque the last 5 seconds
+      ret.steerFaultTemporary = cp.vl["STEER_RATE"]["HANDS_OFF_5_SECONDS"] == 1
 
     self.acc_active_last = ret.cruiseState.enabled
 
